60c73402a178492efe57de6c932bc576ec13ee56
usatashi <usatashi@yahoo.com>
2015-12-14 10:35:36 -0800

    Signed-off-by: usatashi <usatashi@yahoo.com>

--- Project/Project_1/main.cpp
+++ Project/Project_1/main.cpp
@@ -23,14 +23,23 @@ int main(int argc, char** argv) {
     //to be used later, so they can use 1s and 2s for answers
     int answer;
     int roll;
-    //INtroduction statment explaining the rules of the game
+    int size=13;//size of array
+    int count[size]={};//initialize the array
+    int throws=36000;//number of times the dice have been thrown
+    int sum=0;//to add up all the times dice were thrown
+    srand(static_cast<int>(time(0)));
+    
+    //Introduction statment explaining the rules of the game
     cout<<"This is the game of craps! Try to roll the same number twice\n"
             "before you roll a 7, or roll a 7 or eleven on your first roll\n"
             "to win instantly!\n"
             "If on your second roll, you roll a seven at anytime, then you lose "
             "automatically!\n"
-            "Enter 1 to begin and roll, enter any other number to exit at "
+            "Enter 1 to begin and roll, enter 2 to see what your chances"
+            "are, or enter any other number to exit at "
             "any time\n";
+    cin>>answer;
+    if(answer==1){
     do{
     //These are the different dice roll and their combinations
     int roll1,roll2,roll3,roll4,roll5;
@@ -78,11 +87,32 @@ int main(int argc, char** argv) {
                 //ask them if they want to play again
                 }while((answer==1)&&(roll5!=7)&&(roll5!=roll));
                 cout<<"Play again?\n";
-                cout<<"Enter 1 for yes"<<endl;
+                cout<<"Enter 1 for yes, any other number to exit"<<endl;
                 cin>>answer;
     }while(answer==1);
+    }
+    else if (answer==2){
+    cout<<"These are the chances of you rolling different numbers"<<endl;\
+    
+    for (int dice=1;dice<=throws;dice++){
+        int roll1=(rand()%6)+1;
+        int roll2=(rand()%6)+1;
+        roll=roll1+roll2;
+        count[roll]++;
+    }
+    
+    for (int value=2;value<size;value++){
+        sum+=count[value];
+        cout<<value<<" was thrown "
+            <<count[value]<<" times with a "<<(count[value]*100/throws)<<
+                "% chance of rolling"<<endl;
+    };
+    cout<<"Dice were thrown "<<sum<<" times"<<endl;
+    cout<<"Would you like to return to play the game now or quit?"<<endl;
+    cin>>answer;
+    }
     cout<<"Good-bye!"<<endl;
-
+    
     return 0;
 }
 
